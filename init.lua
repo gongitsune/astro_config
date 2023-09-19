c6ad96befa08b39ebda46ef101c46f842f11b2a5
g22307 <g22307@kagawa.kosen-ac.jp>
2023-09-19 21:08:00 +0900

    update

--- init.lua
+++ init.lua
@@ -43,7 +43,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 2000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -51,6 +51,13 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+    },
+    config = {
+      clangd = {
+        capabilities = {
+          offsetEncoding = "utf-8",
+        },
+      },
     },
   },
 
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,20 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "p00f/clangd_extensions.nvim", -- install lsp plugin
+    init = function()
+      -- load clangd extensions when clangd attaches
+      local augroup = vim.api.nvim_create_augroup("clangd_extensions", { clear = true })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = augroup,
+        desc = "Load clangd_extensions with clangd",
+        callback = function(args)
+          if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "clangd" then
+            require "clangd_extensions"
+            -- add more `clangd` setup here as needed such as loading autocmds
+            vim.api.nvim_del_augroup_by_id(augroup) -- delete auto command since it only needs to happen once
+          end
+        end,
+      })
+    end,
+  },
 }
